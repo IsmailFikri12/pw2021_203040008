493fd93e0c2fde6f7dd3002c35ed72f6519fd16c
Ismail Fikri <ismailfikri674@gmail.com>
2021-02-11 10:38:22 +0700

    menyelesaikan video pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,79 @@
+<?php 
+/*
+Ismail Fikri
+203040008
+https://github.com/IsmailFikri12/pw2021_203040008
+pertemuan 2 - 8 Februari 2021
+mempelajari mengenai sintaks PHP 
+*/
+?> 
+
+<?php 
+// Pertemuan 2 - pw2021_203040008
+// Sintaks PHP
+
+// Standar Output
+// echo, print (cetak tulisan dan variabel)
+// print_r (cetak isi array)
+// var_dump (melihat isi variabel)
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// Tidak boleh diawali angka, tapi boleh mengandung angka
+// $nama = "Ismail Fikri";
+// echo "Hallo, nama saya $nama";
+
+// Operator
+// Aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// Penggabung string / concatenation / concat
+// .
+// $nama_depan = "Ismail";
+// $nama_belakang = "Fikri";
+// echo $nama_depan . " " .  $nama_belakang;
+
+// Assignment
+// =, +=, -=, *=, /=, %=, .=,
+// $x = 1;
+// $y -= 5;
+// echo $x;
+// $nama = "Ismail"
+// $nama .= " "
+// $nama .= "Fikri"
+
+// Perbanding
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// Identitas
+// ===, !==
+// var_dump(1 === "1");
+
+// Logika
+// && , ||, ! 
+// $x = 30;
+// var_dump($x < 20 || $x % 2 == 0);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Hallo, Selamat Datang <?php echo "Ismail Fikri"; ?></h1>
+
+    
+</body>
+</html>
